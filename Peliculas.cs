dc9198205fcd4c594d42afbcefc5685872a4dde4
callaito <radelinrsm029@gmail.com>
2024-03-07 16:08:51 -0400

    Se agrega el Proyecto

---
+++ Peliculas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_object_01_22_1_2024
+{
+    internal class Peliculas
+    {
+        public string Nombre { get; set; }
+
+        public string Genero { get; set; }
+
+        public double Duracion { get; set; }
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine($"Nombre:  {Nombre}");
+            Console.WriteLine($"Género:  {Genero}");
+            Console.WriteLine($"Duración:  {Duracion}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using array_object_01_22_1_2024;
+
+Console.WriteLine("GESTION DE PELICULAS Y SERIES");
+Console.WriteLine();
+
+Peliculas[] peli = new Peliculas[3];
+
+for (int i = 0; i < peli.Length; i++)
+{
+    
+
+    peli[i] = new Peliculas();
+    Console.WriteLine("DATOS DE LA PELICULA O SERIE No. " + (i + 1));
+
+    Console.WriteLine("Nombre: ");
+    peli[i].Nombre = Console.ReadLine();
+
+    Console.WriteLine("Gérero: ");
+    peli[i].Genero = Console.ReadLine();
+
+    Console.WriteLine("Duración: ");
+    peli[i].Duracion=double.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+}
+
+//Impirmir los datos
+
+for (int i = 0; i < peli.Length; i++)
+{
+    Console.WriteLine("DATOS DE LA PELICULA O SERIE No. " + (i + 1));
+    Console.WriteLine();
+    Console.WriteLine("========================================");
+    peli[i].ImprimirDatos();
+    Console.WriteLine();
+    Console.WriteLine("========================================");
+
+}
